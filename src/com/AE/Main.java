1c8a17c4c639dec2d9eacac9ab096f4ef2f640af
Angel Rodriguez <arrmixer@gmail.com>
2017-04-04 12:16:12 -0400

    Initial commit

---
+++ src/com/AE/Main.java
@@ -0,0 +1,64 @@
+package com.AE;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+	// write your code here
+
+        int[] myIntegers = getIntegers(5);
+
+
+
+        int[] sortedArray = sortedDescendingArray(myIntegers);
+
+        printArray(sortedArray);
+    }
+
+
+    public static void printArray(int[] array){
+        for(int i = 0;i<array.length;i++){
+            System.out.println("The number entered is " + array[i] + " and it's position is " + i + "." );
+        }
+    }
+
+
+
+    public static int[] getIntegers(int number){
+        System.out.println("Enter " + number + " integer values.\r");
+        int[] values = new int[number];
+
+        for(int i=0;i<values.length;i++){
+            values[i] = scanner.nextInt();
+        }
+
+        return values;
+    }
+    public static int[] sortedDescendingArray(int[] array){
+//        int[] sortedArray = new int[array.length];
+//        for (int i = 0;i<array.length;i++){
+//            sortedArray[i] = array[i];
+//
+//        }
+        int[] sortedArray = Arrays.copyOf(array, array.length);
+        boolean flag = true;
+        int temp;
+        while(flag){
+            flag = false;
+            for(int i=0;i<sortedArray.length-1;i++){
+                if(sortedArray[i] < sortedArray[i+1]) {
+                    temp = sortedArray[i];
+                    sortedArray[i] = sortedArray[i+1];
+                    sortedArray[i+1] = temp;
+                    flag = true;
+                }
+            }
+
+        }
+        return sortedArray;
+    }
+}
